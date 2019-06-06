85a6eac29bb4049ccf950fe117731ed6228f61b3
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2019-06-05 20:26:36 -0500

    Encoding with UTF-8
    - Supports some cute faces (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
    - Fixed spacing issue

--- copy_user_files.py
+++ copy_user_files.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # -----------------------------------------------------------------------------
 # Copyright (c) 2019 Brennan Goewert
 
@@ -139,7 +141,7 @@ def copy(src, dst):
 
 
 def app():
-    print('\n* This script does not copy' +
+    print('\n* This script does not copy ' +
           'anything from the downloads folder. *\n')
     username = getUserName()
     userDir = getUserDir()
