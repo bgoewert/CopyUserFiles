acc321e7cecc5d718fbc7da3ba56d9cd8935066e
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2019-06-14 11:15:54 -0500

    Renaming logfile created by script
    Minor change, keeps it inline with the rename of the script name convention.

--- copyuserfiles.py
+++ copyuserfiles.py
@@ -38,7 +38,7 @@ regKey_UserFolderLocations = (r'Software\Microsoft\Windows\CurrentVersion' +
 
 # Path for log file
 log_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                        'copy_user_files.log')
+                        'copyuserfiles.log')
 # Logging config
 logging.basicConfig(level=logging.INFO,
                     filename=log_path,
